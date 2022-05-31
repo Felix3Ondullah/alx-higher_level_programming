73ca7ab63b923b1e3df393b85208cf6c7d3546e4
Felix3Ondullah <felixondullah@gmail.com>
2022-05-31 13:42:01 +0300

    insert number

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,51 +1,45 @@
-#include "lists.h"
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "lists.h"
 /**
- *insert_node - inserts number in sorte list
- *@head: addres of head
- *@number: number to insert
+ *insert_node - inserts a node into a sorted list
+ *@head: the head of the linked list
+ *@number: the number data of the new node
  *
- *Return: on success addres of new node, NULL if failed
+ *Return: the address of the new node
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *current = *head;
-	listint_t *temp;
-	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *new;
+	listint_t *top;
 
-	if(!head)
-		return (NULL);
+	top = *head;
 
-	if(!new)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
 
 	new->n = number;
+	new->next = NULL;
 
-	if (*head == NULL || number < (*head)->n)
+	if (*head == NULL || top->n > new->n)
 	{
-		temp = *head;
+		new->next = *head;
 		*head = new;
-		new->next = temp;
 		return (new);
 	}
-	while (current)
+	while (top->next != NULL)
 	{
-		if (current->next == NULL)
+		if ((top->next->n > new->n && top->n < new->n)
+		    || new->n == top->n)
 		{
-			current->next == new;
+			new->next = top->next;
+			top->next = new;
 			return (new);
 		}
-		if (number < current->next->)
-		{
-			temp = current->next;
-			current->next = new;
-			new->next = temp;
-			return (new);
-		}
-		current = current->next;
+		top = top->next;
 	}
-	free(new);
-	return (NULL);
+	new->next = top->next;
+	top->next = new;
+	return (new);
 }
