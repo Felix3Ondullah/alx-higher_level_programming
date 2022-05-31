542bdf0b754921cf02606bcf20cc070d438dc522
Felix3Ondullah <felixondullah@gmail.com>
2022-05-31 13:36:19 +0300

    6print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for num1 in range(10):
-    for num2 in range(10):
-        if num2 > num1 and num1 != 8:
-            print('{}{}'.format(num1, num2), end=", ")
-print("{}{}".format(num1 - 1, num2))
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}".format(i, j), end=", ")
